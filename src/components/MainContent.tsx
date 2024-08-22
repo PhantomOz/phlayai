1bfd657458878840708cf163cc479de31fa91dc9
PhantomOz <faniogor@gmail.com>
2024-08-22 04:21:00 +0100

    Add speech to text

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,13 +1,26 @@
 'use client';
 import { IoAttachSharp, IoSend } from "react-icons/io5";
-import { Button, HStack, Textarea, VStack } from "@chakra-ui/react";
+import { FaMicrophone } from "react-icons/fa";
+import { Button, HStack, IconButton, Textarea, VStack } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { model } from "@/connection";
 import MessageComp from "./MessageComp";
+import useSpeechToText from "react-hook-speech-to-text";
 
 export default function MainContent() {
     const [prompt, setPrompt] = React.useState('');
     const [messages, setMessages] = React.useState<any>([]);
+    const {
+        error,
+        interimResult,
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+    } = useSpeechToText({
+        continuous: true,
+        useLegacyResults: false
+    });
 
     React.useEffect(() => {
 
@@ -24,12 +37,42 @@ export default function MainContent() {
         sendPrompt();
     }, [messages]);
 
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            if (isRecording) {
+                setIsAnimating(!isAnimating);
+            }
+        }, 500);
+
+        return () => clearInterval(interval);
+    }, [isAnimating, isRecording]);
+
+    React.useEffect(() => {
+        console.log(interimResult);
+        console.log(results);
+        console.log(error);
+        if (interimResult !== undefined) {
+            setPrompt(interimResult);
+        }
+    }, [interimResult, results, error]);
+
     const handleChange = (e: any) => {
         setPrompt(e.target.value);
     }
 
     const handlePrompting = async () => {
         setMessages([...messages, { message: prompt, isSender: true }]);
+    }
+
+    const handleMic = () => {
+        if (!isRecording) {
+            startSpeechToText();
+        } else {
+            stopSpeechToText();
+            setIsAnimating(false);
+        }
     }
 
     return (
@@ -39,8 +82,9 @@ export default function MainContent() {
             </VStack>
             <HStack alignItems={'center'} w={'80%'} justifyContent={'space-between'} position={'fixed'} bottom={20} p={'10px'} bgColor={'white'} rounded={'lg'}>
                 <IoAttachSharp size={'20px'} />
-                <Textarea rows={1} width={'100%'} resize={'none'} value={prompt} onChange={handleChange} />
-                <Button rightIcon={<IoSend size={'25px'} />} rounded={'lg'} disabled={prompt.length === 0} onClick={handlePrompting} />
+                <Textarea rows={1} width={'100%'} resize={'none'} value={prompt} onChange={handleChange} placeholder={isRecording ? 'Listening' : 'Enter a prompt here'} />
+                <IconButton aria-label="send" icon={<IoSend size={'25px'} />} rounded={'lg'} disabled={prompt.length === 0} onClick={handlePrompting} />
+                <IconButton aria-label="record prompt" color={isAnimating ? 'blue.500' : 'gray[500]'} icon={<FaMicrophone size={'25px'} />} rounded={'lg'} disabled={prompt.length === 0} onClick={handleMic} transform={isAnimating ? 'scale(1.2)' : 'scale(1)'} transition="transform 0.3s ease-in-out" />
             </HStack>
         </VStack >
     )
