641a1dac092595e20273999dc3fea094dd822d27
PhantomOz <faniogor@gmail.com>
2024-08-22 01:26:59 +0100

    Chatbot responsive

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,7 +1,23 @@
+'use client';
 import { IoAttachSharp, IoSend } from "react-icons/io5";
-import { HStack, InputGroup, InputLeftElement, InputRightElement, Textarea, VStack } from "@chakra-ui/react";
+import { Button, HStack, Textarea, VStack } from "@chakra-ui/react";
+import React from "react";
+import { model } from "@/connection";
 
 export default function MainContent() {
+    const [prompt, setPrompt] = React.useState('');
+
+    const handleChange = (e: any) => {
+        setPrompt(e.target.value);
+    }
+
+    const handlePrompting = async () => {
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        console.log(text);
+    }
+
     return (
         <VStack h={'100vh'} position={'relative'} width={'100%'}>
             <VStack>
@@ -9,8 +25,8 @@ export default function MainContent() {
             </VStack>
             <HStack alignItems={'center'} w={'80%'} justifyContent={'space-between'} position={'fixed'} bottom={0} p={'10px'} bgColor={'white'} rounded={'lg'}>
                 <IoAttachSharp size={'20px'} />
-                <Textarea rows={1} width={'100%'} resize={'none'} />
-                <IoSend size={'25px'} />
+                <Textarea rows={1} width={'100%'} resize={'none'} value={prompt} onChange={handleChange} />
+                <Button rightIcon={<IoSend size={'25px'} />} rounded={'lg'} disabled={prompt.length === 0} onClick={handlePrompting} />
             </HStack>
         </VStack>
     )
--- src/connection/index.ts
+++ src/connection/index.ts
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_API_KEY);
 
 export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
